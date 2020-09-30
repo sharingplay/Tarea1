178815b490dcd538ef094bb952f8895159600c3a
Fabian <ramirezfabian003@gmail.com>
2020-09-29 22:08:05 -0600

    Creado componente Subtitle

--- Tarea1/src/app/components/subtitle/subtitle.component.ts
+++ Tarea1/src/app/components/subtitle/subtitle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input , OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-subtitle',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubtitleComponent implements OnInit {
 
+  @Input() text;
   constructor() { }
 
   ngOnInit(): void {

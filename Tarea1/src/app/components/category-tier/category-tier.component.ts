870b34c04111b8c0d09d2c267380ad0ddbef6f5a
Fabian <ramirezfabian003@gmail.com>
2020-09-30 16:09:50 -0600

    Crea categorias de forma escalable

--- Tarea1/src/app/components/category-tier/category-tier.component.ts
+++ Tarea1/src/app/components/category-tier/category-tier.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category-tier',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryTierComponent implements OnInit {
 
+  @Input() id;
+  @Input() name;
   constructor() { }
 
   ngOnInit(): void {
--- Tarea1/src/app/views/gestion-categorias/gestion-categorias.component.ts
+++ Tarea1/src/app/views/gestion-categorias/gestion-categorias.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestion-categorias.component.scss']
 })
 export class GestionCategoriasComponent implements OnInit {
-
+  categories = [['Verduras', 'ver01'], ['Carnes', 'car01'], ['Legumbres', 'leg01'], ['Mariscos', 'mar01'], ['Hortalizas', 'hor01']];
   constructor() { }
 
   ngOnInit(): void {

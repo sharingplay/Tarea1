ee9ed7b0c4a1350e5c8d0473d7147a621be934a9
Wajib Zaglul <wajib10@gmail.com>
2020-10-12 15:35:01 -0600

    Detalles pedidos a JSON

--- Tarea1/src/app/views/vista-productores/gestion-pedidos/detalles-pedidos/detalles-pedidos.component.ts
+++ Tarea1/src/app/views/vista-productores/gestion-pedidos/detalles-pedidos/detalles-pedidos.component.ts
@@ -2,6 +2,8 @@ import {Component, Inject, OnInit} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
+import {JsonArray} from '@angular/compiler-cli/ngcc/src/packages/entry_point';
+import {HttpClientService} from '../../../../services/http-client-service';
 // import {GuardService}from '.././guard.service';
 declare const fileDropArea: any;
 declare const myTest: any;
@@ -15,7 +17,7 @@ let flag = true;
 })
 export class DetallesPedidosComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<DetallesPedidosComponent>,
-              @Inject(MAT_DIALOG_DATA) public message: string[]) {console.log(message); }
+              @Inject(MAT_DIALOG_DATA) public message: HttpClientService['pedidos']) {console.log(message); }
 
   // tslint:disable-next-line:typedef
   onClicka(){

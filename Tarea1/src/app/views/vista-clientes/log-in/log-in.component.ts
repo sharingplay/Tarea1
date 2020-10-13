218bf75499ed634418da23f6cc93a89d0eb3373f
Mariana Vargas Ramirez <marianavargas11209@gmail.com>
2020-10-12 21:44:17 -0600

    [edit] login obtiene datos

--- Tarea1/src/app/views/vista-clientes/log-in/log-in.component.ts
+++ Tarea1/src/app/views/vista-clientes/log-in/log-in.component.ts
@@ -6,8 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
+  private username: string;
+  private password: string;
 
   constructor() { }
+  login(): void{
+
+    this.username = (document.getElementById('username') as HTMLInputElement).value;
+    this.password = (document.getElementById('password') as HTMLInputElement).value;
+    console.log(this.username);
+    console.log(this.password);
+
+  }
 
   ngOnInit(): void {
   }

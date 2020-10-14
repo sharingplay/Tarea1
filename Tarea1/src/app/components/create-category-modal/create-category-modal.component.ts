1ef4ec04d9b778110a9463d0896f69220dcc4cfa
Fabian <ramirezfabian003@gmail.com>
2020-10-13 21:07:39 -0600

    Agrega Categorias

--- Tarea1/src/app/components/create-category-modal/create-category-modal.component.ts
+++ Tarea1/src/app/components/create-category-modal/create-category-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {BsModalRef} from 'ngx-bootstrap/modal';
+import {HttpClientService} from '../../services/http-client-service';
 
 @Component({
   selector: 'app-create-category-modal',
@@ -8,9 +9,17 @@ import {BsModalRef} from 'ngx-bootstrap/modal';
 })
 export class CreateCategoryModalComponent implements OnInit {
 
-  constructor(public bsModalRef: BsModalRef) { }
+  constructor(public bsModalRef: BsModalRef, private httpClient: HttpClientService) { }
 
   ngOnInit(): void {
   }
-
+  sendCategory(): void{
+    // tslint:disable-next-line:prefer-const
+    console.log('SDaasdasdasd');
+    const nuevaCategoria = {
+      Codigo: (document.getElementById('id') as HTMLInputElement).value,
+      Nombre: (document.getElementById('name') as HTMLInputElement).value,
+    };
+    this.httpClient.post('http://localhost:5000/api/Categorias/insert', nuevaCategoria);
+  }
 }

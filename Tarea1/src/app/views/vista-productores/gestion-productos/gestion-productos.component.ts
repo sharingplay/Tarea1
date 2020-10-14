3968e96097ee448a9ae962f1ad4ee9b983f14166
Mariana Vargas Ramirez <marianavargas11209@gmail.com>
2020-10-14 16:35:07 -0600

    [edit] gestion productos

--- Tarea1/src/app/views/vista-productores/gestion-productos/gestion-productos.component.ts
+++ Tarea1/src/app/views/vista-productores/gestion-productos/gestion-productos.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {MessengerService} from "../../../MessengerService";
+import {HttpClientService} from '../../../services/http-client-service';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-gestion-productos',
@@ -9,13 +12,20 @@ import {MessengerService} from "../../../MessengerService";
 export class GestionProductosComponent implements OnInit {
   cliente: any;
 
-  constructor(private messengerService: MessengerService) {
-    console.log("EN ACTUALIZAR")
-    this.messengerService.message.subscribe(value => {
-      console.log(value);
-      this.cliente = value;
-    });
+  constructor(private ruta: ActivatedRoute, public httpService: HttpClientService,
+              @Inject(MAT_DIALOG_DATA) public message: HttpClientService['productos']) {
+
   }
+  actualizarDatos(): void {
+    this.message.nombre = (document.getElementById('producto') as HTMLInputElement).value;
+    this.message.categoria = (document.getElementById('categoria') as HTMLInputElement).value;
+    this.message.modo = (document.getElementById('Mododeventa') as HTMLInputElement).value;
+    this.message.disponibilidad = (document.getElementById('DisponibilidadProducto') as HTMLInputElement).value;
+    this.message.precio = (document.getElementById('precio') as HTMLInputElement).value;
+    console.log(this.message);
+    this.httpService.post('https://localhost:5001/api/Productos/modify', this.message);
+  }
+
 
   ngOnInit(): void {
   }

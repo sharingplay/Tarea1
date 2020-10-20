61419b77174ee31864685b513642d04c77159cba
Mario Araya <marioaxtrem@gmail.com>
2020-10-19 18:39:39 -0600

    cualquier vara

--- server/server/Models/Afiliaciones.cs
+++ server/server/Models/Afiliaciones.cs
@@ -15,26 +15,6 @@
     public string Usuario { get; set; }
     public string Password { get; set; }
 
-    public Afiliaciones()
-    {
-        public string Cedula { get; set; }
-        public string Nombre { get; set; }
-        public string Apellidos { get; set; }
-        public string Direccion { get; set; }
-        public string Provincia { get; set; }
-        public string Canton { get; set; }
-        public string Distrito { get; set; }
-        public string Nacimiento { get; set; }
-        public string Telefono { get; set; }
-        public string SINPE { get; set; }
-        public string PuntosEntrega { get; set; }
-        public string Pedidos { get; set; }
-        public string Usuario { get; set; }
-        public string Password { get; set; }
-
-        public Afiliaciones()
-        {
-        }
 
         public Afiliaciones(string productProvincia, string productCanton, string productDistrito, string productCedula, string productNombre, string productApellido, string productDireccion, string productNacimiento, string productTelefono, string productSINPE, string productPedidos, string productUsuario, string productPassword)
         {
@@ -53,4 +33,3 @@
             Password = productPassword;
         }
     }
-}
